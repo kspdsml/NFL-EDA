c9843ccb37f63784231a4e7ab0041ae1a3f9d325
Kishan Patel <kspdsml@gmail.com>
2023-06-09 00:12:44 -0400

    Added visualization tool for scatter plots

--- app.py
+++ app.py
@@ -16,12 +16,30 @@ This app performs simple webscraping of NFL Football player stats data!
 st.sidebar.header('User Input Features')
 selected_year = st.sidebar.selectbox('Year', range(2022, 1990, -1))
 selected_category = st.sidebar.selectbox('Category', ['passing', 'rushing', 'receiving'])
-
+numerical_stats = {
+    'passing': ['Age', 'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'Int', 'Lng', 'Y/A', 'Y/G', 'Rate'],
+    'rushing': ['Age', 'Att', 'Yds', 'TD', '1D', 'Lng', 'Y/A', 'Y/G', 'Fmb'],
+    'receiving': ['Age','Tgt', 'Rec', 'Ctch%', 'Yds', 'Y/R', 'TD', '1D', 'Lng', 'Y/Tgt', 'R/G', 'Y/G', 'Fmb']
+}
 # Web scraping of NFL player stats
 # https://www.pro-football-reference.com/years/2019/rushing.htm
 @st.cache_data
 def load_data(year, category):
     if category == 'rushing':
+        # Column labels for rushing category
+        # column_labels = {
+        #     'Age': 'Age',
+        #     'G': 'Games',
+        #     'GS': 'Games Started',
+        #     'Att': 'Attempts',
+        #     'Yds': 'Yards',
+        #     'TD': 'Touchdowns',
+        #     '1D': 'First Downs',
+        #     'Lng': 'Longest',
+        #     'Y/A': 'Yards per Attempt',
+        #     'Y/G': 'Yards per Game',
+        #     'Fmb': 'Fumbles'
+        # }
 
         url = "https://www.pro-football-reference.com/years/" + str(year) + "/" + category + ".htm"
         html = pd.read_html(url, header = 1)
@@ -29,14 +47,39 @@ def load_data(year, category):
         raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
         raw = raw.fillna(0)
         playerstats = raw.drop(['Rk'], axis=1)
+        columns_to_convert = ['Age', 'G', 'GS', 'Att', 'Yds', 'TD', '1D', 'Lng', 'Y/A', 'Y/G', 'Fmb']
+        playerstats[columns_to_convert] = playerstats[columns_to_convert].apply(pd.to_numeric, errors='coerce', downcast='integer')
+        # playerstats = playerstats.rename(columns=column_labels)
         return playerstats
+    
     elif category == 'passing':
+        # Columns labels for passing category 
+        # column_labels = {
+        #     'Age': 'Age',
+        #     'G': 'Games',
+        #     'GS': 'Games Started',
+        #     'Cmp': 'Completions',
+        #     'Att': 'Attempts',
+        #     'Cmp%': 'Completion Percentage',
+        #     'Yds': 'Yards',
+        #     'TD': 'Touchdowns',
+        #     'Int': 'Interceptions',
+        #     'Lng': 'Longest Completion',
+        #     'Y/A': 'Yards per Attempt',
+        #     'Y/C': 'Yards per Completion',
+        #     'Y/G': 'Yards per Game',
+        #     'Rate': 'Quarterback Rating'}
         url = "https://www.pro-football-reference.com/years/" + str(year) + "/" + category + ".htm"
         html = pd.read_html(url)
         df = html[0]
         raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
         raw = raw.fillna(0)
         playerstats = raw.drop(['Rk'], axis=1)
+        playerstats = playerstats.drop(['Yds.1', 'QBrec', 'TD%', 'Int%', 'AY/A', 'QBR', 'Sk%', 'NY/A', 'ANY/A', '4QC', 'GWD'], axis=1)
+        columns_to_convert = ['Age', 'G', 'GS', 'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'Int', 'Lng', 'Y/A', 'Y/G', 'Rate']
+        playerstats[columns_to_convert] = playerstats[columns_to_convert].apply(pd.to_numeric, errors='coerce', downcast='integer')
+        # playerstats = playerstats.rename(columns=column_labels)
+
         return playerstats
     
     elif category == 'receiving':
@@ -46,8 +89,12 @@ def load_data(year, category):
         raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
         raw = raw.fillna(0)
         playerstats = raw.drop(['Rk'], axis=1)
+        columns_to_convert = ['Age', 'G', 'GS', 'Tgt', 'Rec', 'Ctch%', 'Yds', 'Y/R', 'TD', '1D', 'Lng', 'Y/Tgt', 'R/G', 'Y/G', 'Fmb']
+        playerstats[columns_to_convert] = playerstats[columns_to_convert].apply(pd.to_numeric, errors='coerce', downcast='integer')
         return playerstats
-    
+
+
+
 playerstats = load_data(selected_year, selected_category)
 
 
@@ -59,13 +106,25 @@ selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique
 unique_pos = ['RB','QB','WR','FB','TE']
 selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
 
+# # Sidebar - Stat selection
+# with st.sidebar:
+#     st.write('Pick stat and range')
+#     selected_stat = st.selectbox("", ('Age', 'G', 'GS', 'Att', 'Yds', 'TD', '1D', 'Lng', 'Y/A', 'Y/G', 'Fmb'))
+#     # Convert minimum and maximum values to integers
+#     start_stat = int(playerstats[selected_stat].min())-1
+#     end_stat = int(playerstats[selected_stat].max())+1
+#     start_range, end_range = st.select_slider(
+#         '',
+#         options= [str(num) for num in range(start_stat, end_stat)],
+#         value=('33', '44')
+#     )
 # Filtering data
 df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
 
-st.header('Display Player Stats of Selected Team(s)')
+st.header(f'Displaying {selected_year} NFL {selected_category.capitalize()} Stats')
 st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
 
-st.dataframe(df_selected_team)
+st.dataframe(df_selected_team,)
 
 # Download NBA player stats data
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
@@ -77,6 +136,25 @@ def filedownload(df):
 
 st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
 
+def generate_plot(x, y, data, visualization):
+    if visualization == 'Scatter Plot':
+        sns.set_style('darkgrid')
+        sns.set_color_codes("pastel")
+        sns.scatterplot(x=x, y=y, data=data)
+        plt.title(f'{visualization} of {y} vs {x}')
+        st.pyplot()
+
+with st.container():
+    st.write('Select graph type and data to compare:')
+    visualization_options = ['Bar Chart', 'Scatter Plot', 'Box Plot']
+    selected_visualization = st.selectbox('Visualization', visualization_options)
+    stat_options = numerical_stats[selected_category]
+    selected_x = st.selectbox('x var', stat_options)
+    selected_y = st.selectbox('y var', stat_options)
+    if st.button('Generate'):
+        generate_plot(selected_x, selected_y, df_selected_team, selected_visualization)
+    if st.button('Clear'):
+        pass
 # Heatmap
 # if st.button('Intercorrelation Heatmap'):
 #     st.header('Intercorrelation Matrix Heatmap')
